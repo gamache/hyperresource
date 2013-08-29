a419ef9d038d1cb1f3c0a1292d1104935acdcfcf
Étienne Barrié <etienne.barrie@gmail.com>
2013-08-29 16:49:17 +0200

    Handle array of Link Objects

--- lib/hyper_resource/links.rb
+++ lib/hyper_resource/links.rb
@@ -10,7 +10,13 @@ class HyperResource
     def init_from_hal(hal_resp)
       return unless hal_resp['_links']
       hal_resp['_links'].each do |rel, link_spec|
-        self[rel] = new_link_from_spec(link_spec)
+        if link_spec.is_a? Array
+          self[rel] = link_spec.map do |link|
+            new_link_from_spec(link)
+          end
+        else
+          self[rel] = new_link_from_spec(link_spec)
+        end
         create_methods_for_link_rel(rel) unless self.respond_to?(rel.to_sym)
       end
     end
