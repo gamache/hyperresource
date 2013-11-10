73f0cb556558b43d31d0837ee418e2559fd89b80
pete gamache <pete@gamache.org>
2013-11-10 13:06:47 -0500

    Adapter docs

--- lib/hyper_resource/adapter.rb
+++ lib/hyper_resource/adapter.rb
@@ -1,22 +1,27 @@
 class HyperResource
+
+  ## HyperResource::Adapter is the interface/abstract base class for 
+  ## adapters to different hypermedia formats (e.g., HAL+JSON).  New
+  ## adapters must implement the public methods of this class.
+
   class Adapter
     class << self
 
-      ## Serialize the object into a string.
+      ## Serialize the given object into a string.
       def serialize(object)
         raise NotImplementedError, "This is an abstract method -- subclasses "+
           "of HyperResource::Adapter must implement it."
       end
 
-      ## Deserialize a string into an object.
+      ## Deserialize a given string into an object (Hash).
       def deserialize(string)
         raise NotImplementedError, "This is an abstract method -- subclasses "+
           "of HyperResource::Adapter must implement it."
       end
 
-      ## Apply a response object (generally parsed JSON or XML) to the given
-      ## HyperResource object.  Returns the updated resource.
-      def apply(object, resource, opts={})
+      ## Use a given deserialized response object (Hash) to update a given
+      ## resource (HyperResource), returning the updated resource.
+      def apply(response, resource, opts={})
         raise NotImplementedError, "This is an abstract method -- subclasses "+
           "of HyperResource::Adapter must implement it."
       end
