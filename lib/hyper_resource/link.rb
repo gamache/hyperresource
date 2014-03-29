648ce8c229058158fc172585cc34d414f6010f90
pete gamache <pete@gamache.org>
2014-03-29 08:38:37 -0400

    Link refactor

--- lib/hyper_resource/link.rb
+++ lib/hyper_resource/link.rb
@@ -6,33 +6,62 @@ require 'hyper_resource/modules/http'
 class HyperResource
 
   ## HyperResource::Link is an object to represent a hyperlink and its
-  ## URL or body parameters.  It automates loading of the resource
-  ## it points to: `method_missing` loads a resource, then repeats the
-  ## method call on it.
+  ## URL or body parameters, and to encapsulate HTTP calls involving this
+  ## link.  Links are typically created by HyperResource, not by end users.
+  ##
+  ## HTTP method calls return the response as a HyperResource (or subclass)
+  ## object.  Calling an unrecognized method on a link will automatically
+  ## load the resource pointed to by this link, and repeat the method call
+  ## on the resource object.
+  ##
+  ## A HyperResource::Link requires the resource it is based upon to remain
+  ## in scope.  In practice this is rarely a problem, as links are almost
+  ## always accessed through the resource object.
+
   class Link
 
     include HyperResource::Modules::HTTP
 
-    attr_accessor :base_href,
-                  :name,
-                  :templated,
-                  :params,
-                  :default_method
+    ## The literal href of this link; may be templated.
+    attr_accessor :base_href
 
-    ## This is a WeakRef so that HyperResource objects don't leak.
-    attr_accessor :resource # @private
+    ## An optional name describing this link.
+    attr_accessor :name
 
-    ## Returns true if this link is templated.
-    def templated?; templated end
+    ## `true` if this link's href is a URI Template, `false` otherwise.
+    attr_accessor :templated
 
-    ## +link_spec+ must have +href+ defined
+    ## A hash of URL or request body parameters.
+    attr_accessor :params
+
+    ## Default HTTP method for implicit loading.
+    attr_accessor :default_method
+
+    ## A weak reference (`WeakRef`) to the resource from which this link
+    ## originates.  Resource object must still be in scope.
+    def resource
+      ## This is a WeakRef so that HyperResource objects don't leak.
+      @rsrc_ref
+    end
+
+    # @private
+    def resource=(rsrc)
+      ## Use a WeakRef so that HyperResource objects don't leak.
+      real_rsrc = rsrc.is_a?(WeakRef) ? rsrc.__getobj__ : rsrc
+      @rsrc_ref = WeakRef.new(real_rsrc)
+    end
+
+    ## Returns a link based on the given resource and link specification
+    ## hash.  `link_spec` keys are: `href` (string, required), `templated`
+    ## (boolean), `params` (hash), and `default_method` (string, default
+    ## `"get"`).
     def initialize(resource, link_spec={})
       unless link_spec.kind_of?(Hash)
         raise ArgumentError, "link_spec must be a Hash (got #{link_spec.inspect})"
       end
       link_spec = Hash[ link_spec.map{|(k,v)| [k.to_s, v]} ] ## stringify keys
 
-      self.resource = resource.is_a?(WeakRef) ? resource : WeakRef.new(resource)
+      self.resource = resource
       self.base_href = link_spec['href']
       self.name = link_spec['name']
       self.templated = !!link_spec['templated']
@@ -42,7 +71,7 @@ class HyperResource
 
     ## Returns this link's href, applying any URI template params.
     def href
-      if self.templated?
+      if self.templated
         filtered_params = self.resource.outgoing_uri_filter(params)
         URITemplate.new(self.base_href).expand(filtered_params)
       else
@@ -50,6 +79,8 @@ class HyperResource
       end
     end
 
+    ## Returns this link's fully resolved URL, or nil if `resource.root`
+    ## or `href` are malformed.
     def url
       begin
         URI.join(self.resource.root, self.href)
@@ -62,7 +93,7 @@ class HyperResource
     ## itself with the given params applied.
     def where(params)
       params = Hash[ params.map{|(k,v)| [k.to_s, v]} ]
-      self.class.new(self.resource.__getobj__,
+      self.class.new(self.resource,
                      'href' => self.base_href,
                      'name' => self.name,
                      'templated' => self.templated,
@@ -70,8 +101,9 @@ class HyperResource
                      'method' => self.default_method)
     end
 
-    ## If we were called with a method we don't know, load this resource
-    ## and pass the message along.  This achieves implicit loading.
+    ## Unrecognized methods invoke an implicit load of the resource pointed
+    ## to by this link.  The method call is then repeated on the returned
+    ## resource.
     def method_missing(method, *args)
       self.send(default_method || :get).send(method, *args)
     end
